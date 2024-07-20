4c2a12831ee742da07046ed410cc811c750bd0d6
PierreSwtich <skamper28@gmail.com>
2024-07-20 02:05:19 +0200

    feat(public): add static html page

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,29 @@ import swaggerUi from 'swagger-ui-express';
 import specs from './swagger';
 import dotenv from 'dotenv';
 import routes from './routes';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+// Serve static files from 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use('/api', routes);
 
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
+  console.log(`API running on http://localhost:${port}/api/books`);
   console.log(`Swagger UI available at http://localhost:${port}/api-docs`);
+  console.log(`Static HTML available at http://localhost:${port}/`)
 });

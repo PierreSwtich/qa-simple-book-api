0c6093bd2277797a7aafe2c7b2a81ae4747200c1
PierreSwtich <skamper28@gmail.com>
2024-07-20 02:23:34 +0200

    fix(vercel): configuration

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,26 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import specs from './swagger';
 import dotenv from 'dotenv';
-import routes from './routes';
 import path from 'path';
+import routes from './routes';
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-
-// Serve Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-
-// Serve static files
+// Middleware to serve static files
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Swagger UI route
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+// API routes
 app.use('/api', routes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
-  console.log(`API running on http://localhost:${port}/api/books`);
   console.log(`Swagger UI available at http://localhost:${port}/api-docs`);
+  console.log(`API served on http://localhost:${port}/api/books`);
   console.log(`Static HTML available at http://localhost:${port}/`)
 });

0895a4a8eea7c65700aab4087169bb7f9bb57ab9
PierreSwtich <skamper28@gmail.com>
2024-07-22 13:56:37 +0200

    fix(swagger): path

--- src/swagger.ts
+++ src/swagger.ts
@@ -20,7 +20,7 @@ const options = {
       }
     ]
   },
-  apis: [path.join(__dirname, './routes.ts')], // paths to files containing OpenAPI annotations
+  apis: ['src/routes.ts'], // paths to files containing OpenAPI annotations
 };
 
 const specs = swaggerJSDoc(options);

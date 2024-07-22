7b4db13aebca3ba651202cd8d6e86768c1337611
PierreSwtich <skamper28@gmail.com>
2024-07-22 14:26:34 +0200

    test8

--- src/swagger.ts
+++ src/swagger.ts
@@ -20,7 +20,8 @@ const options = {
       }
     ]
   },
-  apis: [path.join(process.cwd(), 'src', 'routes.ts')], // paths to files containing OpenAPI annotations
+  apis: [path.join(process.cwd(), 'src', 'routes.ts')],
+  paths: [path.join(process.cwd(), 'src', 'routes.ts')] // paths to files containing OpenAPI annotations
 };
 
 console.log(`Swagger configuration: ${JSON.stringify(options, null, 2)}`);

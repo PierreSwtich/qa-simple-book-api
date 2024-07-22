bf31b789fffb754212b614ce34a9d2a76f57439a
PierreSwtich <skamper28@gmail.com>
2024-07-22 11:29:29 +0200

    fix(swagger): add remote url

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,21 +1,25 @@
-import swaggerJSDoc, { Options } from 'swagger-jsdoc';
+import swaggerJSDoc from 'swagger-jsdoc';
 
-const options: Options = {
+const options = {
   definition: {
     openapi: '3.0.0',
     info: {
       title: 'Book API',
       version: '1.0.0',
-      description: 'API documentation for the Book API',
+      description: 'API documentation for the Book API'
     },
     servers: [
       {
-        url: 'http://localhost:3000/api',
-        description: 'Development server',
+        url: 'https://qa-simple-book-api.vercel.app/api',
+        description: 'Production server'
       },
-    ],
+      {
+        url: 'http://localhost:3000/api',
+        description: 'Development server'
+      }
+    ]
   },
-  apis: ['./src/routes.ts'], // Path to the API docs
+  apis: ['./src/routes.ts'], // paths to files containing OpenAPI annotations
 };
 
 const specs = swaggerJSDoc(options);
